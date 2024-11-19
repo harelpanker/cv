6f678ffa15d0a87c78c6b5cd866afcfe1dba4397
Harel Panker <harelpanker@gmail.com>
2024-11-19 20:40:52 +0100

    first commit

---
+++ components/layout/fonts.ts
@@ -0,0 +1,7 @@
+import localFont from 'next/font/local';
+
+export const general_sans = localFont({
+    src: '../../public/fonts/general_sans.woff2',
+    display: 'swap',
+    variable: '--font-general_sans',
+  })
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,8 @@ export default {
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      fontFamily: {
+        general_sans: ['var(--font-general_sans)'],
       },
     },
   },
