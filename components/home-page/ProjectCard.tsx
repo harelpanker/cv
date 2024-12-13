f1162e366a58653cac3f8b56eb5a054de19497cb
Harel Panker <harelpanker@gmail.com>
2024-12-13 21:18:36 +0100

    fix build

---
+++ components/home-page/ProjectCard.tsx
@@ -0,0 +1,34 @@
+import Image from 'next/image';
+import { Title } from '@/components/ui/title';
+import { Project } from '@/lib/projects-data';
+import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card';
+import styles from '@/styles/content-layout.module.css';
+import arrow from '@/public/icons/arrow-up.svg';
+
+export default function ProjectCard({ project }: { project: Project }) {
+	return (
+		<li>
+			<a href={project.fullUrl} target='_blank' rel='noopener noreferrer'>
+				<CardContainer>
+					<CardBody className='flex flex-col gap-y-4'>
+						{project.src ? (
+							<CardItem as='figure' translateZ='50' className='overflow-hidden rounded-2xl'>
+								<Image src={project.src} alt={project.name} />
+							</CardItem>
+						) : null}
+						<div className='flex justify-between gap-x-4'>
+							<CardItem translateZ='30'>
+								<Title className={styles.title3} level={3}>
+									{project.showName}
+								</Title>
+							</CardItem>
+							<CardItem translateZ='20'>
+								<Image className='opacity-80 invert' src={arrow} alt='arrow' />
+							</CardItem>
+						</div>
+					</CardBody>
+				</CardContainer>
+			</a>
+		</li>
+	);
+}
--- components/home-page/Projects.tsx
+++ components/home-page/Projects.tsx
@@ -1,11 +1,9 @@
 import CVLayout from './CVLayout';
 import { Title } from '@/components/ui/title';
 import { Text } from '@/components/ui/text';
-import { CardBody, CardContainer, CardItem } from '@/components/ui/3d-card';
+import ProjectCard from '@/components/home-page/ProjectCard';
 import styles from '@/styles/content-layout.module.css';
 import { data } from '@/lib/projects-data';
-import Image from 'next/image';
-import arrow from '@/public/icons/arrow-up.svg';
 
 export default function Projects() {
 	return (
@@ -17,33 +15,21 @@ export default function Projects() {
 				I&apos;m out the gate, I&apos;m two tons, tunnel vision, I go get &apos;em Atittude like a black rhino, I&apos;m
 				building my momentum
 			</Text>
-			<ul className='mt-4 flex flex-col gap-y-12'>
+			<ul className='mb-12 mt-4 flex flex-col gap-y-12'>
 				{data
 					.filter((item) => item.src)
 					.map((item) => (
-						<li key={item.name}>
-							<a href={item.fullUrl} target='_blank' rel='noopener noreferrer'>
-								<CardContainer>
-									<CardBody className='flex flex-col gap-y-4'>
-										{item.src ? (
-											<CardItem as='figure' translateZ='50' className='overflow-hidden rounded-2xl'>
-												<Image src={item.src} alt={item.name} />
-											</CardItem>
-										) : null}
-										<div className='flex justify-between gap-x-4'>
-											<CardItem translateZ='30'>
-												<Title className={styles.title3} level={3}>
-													{item.showName}
-												</Title>
-											</CardItem>
-											<CardItem translateZ='20'>
-												<Image className='opacity-80 invert' src={arrow} alt='arrow' />
-											</CardItem>
-										</div>
-									</CardBody>
-								</CardContainer>
-							</a>
-						</li>
+						<ProjectCard key={item.name} project={item} />
+					))}
+			</ul>
+			<Title className={styles.title3} level={4}>
+				And many more...
+			</Title>
+			<ul className='flex flex-col gap-y-12'>
+				{data
+					.filter((item) => !item.src)
+					.map((item) => (
+						<li key={item.name}></li>
 					))}
 			</ul>
 		</CVLayout>
