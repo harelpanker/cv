8e78ba73791ee3ee4a9e982f3dc1b31f73dd639e
Harel Panker <harelpanker@gmail.com>
2024-12-14 19:24:47 +0100

    fix build errors

--- components/ui/evervault-card.tsx
+++ components/ui/evervault-card.tsx
@@ -5,18 +5,19 @@ import { useMotionTemplate, motion } from 'framer-motion';
 import { cn } from '@/lib/utils';
 
 export const EvervaultCard = ({ text, className }: { text?: string; className?: string }) => {
-	let mouseX = useMotionValue(0);
-	let mouseY = useMotionValue(0);
+	const mouseX = useMotionValue(0);
+	const mouseY = useMotionValue(0);
 
 	const [randomString, setRandomString] = useState('');
 
 	useEffect(() => {
-		let str = generateRandomString(1500);
+		const str = generateRandomString(1500);
 		setRandomString(str);
 	}, []);
 
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	function onMouseMove({ currentTarget, clientX, clientY }: any) {
-		let { left, top } = currentTarget.getBoundingClientRect();
+		const { left, top } = currentTarget.getBoundingClientRect();
 		mouseX.set(clientX - left);
 		mouseY.set(clientY - top);
 
@@ -45,9 +46,10 @@ export const EvervaultCard = ({ text, className }: { text?: string; className?: 
 	);
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export function CardPattern({ mouseX, mouseY, randomString }: any) {
-	let maskImage = useMotionTemplate`radial-gradient(250px at ${mouseX}px ${mouseY}px, white, transparent)`;
-	let style = { maskImage, WebkitMaskImage: maskImage };
+	const maskImage = useMotionTemplate`radial-gradient(250px at ${mouseX}px ${mouseY}px, white, transparent)`;
+	const style = { maskImage, WebkitMaskImage: maskImage };
 
 	return (
 		<div className='pointer-events-none'>
@@ -76,6 +78,7 @@ export const generateRandomString = (length: number) => {
 	return result;
 };
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const Icon = ({ className, ...rest }: any) => {
 	return (
 		<svg
