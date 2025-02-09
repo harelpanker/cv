6859ff8450cea56a8ce8113ab2aa1f93de923972
Harel Panker <harelpanker@gmail.com>
2025-02-09 13:54:19 +0100

    text change

--- components/home-page/WorkExperience.tsx
+++ components/home-page/WorkExperience.tsx
@@ -1,8 +1,38 @@
 import CVLayout from '@/components/home-page/CVLayout';
 import { Title } from '@/components/ui/title';
 import { Text } from '@/components/ui/text';
+import { IconPointFilled } from '@tabler/icons-react';
 import styles from '@/styles/content-layout.module.css';
 import WorkExperienceLogos from '@/components/home-page/WorkExperienceLogos';
+
+type DataProps = {
+	id: number;
+	isStrong: boolean;
+	text: string;
+};
+
+const data: DataProps[] = [
+	{
+		id: 1,
+		isStrong: false,
+		text: 'Built and maintained websites for clients, collaborating closely with marketing teams and designers.',
+	},
+	{
+		id: 2,
+		isStrong: false,
+		text: 'Developed user-friendly, performance-optimized websites, with a focus on scalability and maintainability.',
+	},
+	{
+		id: 3,
+		isStrong: false,
+		text: 'Delivered SEO-optimized, visually compelling solutions tailored to each client&apos;s needs.',
+	},
+	{
+		id: 4,
+		isStrong: true,
+		text: 'Consistently achieved greater web exposure and improved online visibility for clients, contributing to their business growth.',
+	},
+];
 
 export default function WorkExperience() {
 	return (
@@ -10,15 +40,18 @@ export default function WorkExperience() {
 			<Title className={styles.title3} level={3}>
 				Independent developer | 2019 - PRESENT
 			</Title>
-			<Text>
-				For the past few years, I have been building and maintaining clients websites, working closely with marketing
-				teams and designers.
-			</Text>
-			<Text>
-				The projects I have worked on are a mix of static websites and dynamic web applications built with no-code
-				tools, and <strong className={styles.strong}>NextJS</strong>.
-			</Text>
-			<Text>Clients I&apos;m proudly working with:</Text>
+			<ul className={styles.ul}>
+				{data.map(({ id, isStrong, text }) => (
+					<li key={id}>
+						<IconPointFilled />
+						<Text>{isStrong ? <strong className={styles.strong}>{text}</strong> : text}</Text>
+					</li>
+				))}
+			</ul>
+
+			<Title className='!~text-lg/2xl' level={3}>
+				Clients I&apos;m proudly working with:
+			</Title>
 			<WorkExperienceLogos />
 		</CVLayout>
 	);
