1062d0c1982cc9775af37854e28c386ac2f6a39c
Harel Panker <harelpanker@gmail.com>
2025-02-11 20:54:45 +0100

    changing url

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import { Children } from '@/types/children';
 import { general_sans } from '@/components/layout/fonts';
 import BackgroundAnimation from '@/components/ui/background-animation';
 
+const SITE_URL = 'https://cv.panker.dev';
+
 export const metadata: Metadata = {
 	title: 'Harel Panker | Frontend Developer',
 	description:
@@ -25,7 +27,7 @@ export const metadata: Metadata = {
 		'Web Development',
 		'UI Development',
 	],
-	metadataBase: new URL('https://www.panker.dev/'),
+	metadataBase: new URL(SITE_URL),
 	alternates: {
 		canonical: '/',
 		languages: {
@@ -37,26 +39,15 @@ export const metadata: Metadata = {
 		title: 'Harel Panker | Frontend Developer',
 		description:
 			'Expert frontend developer specializing in React, Next.js, and modern web technologies. Creating responsive, performant, and user-friendly web applications.',
-		url: 'https://www.panker.dev',
+		url: SITE_URL,
 		siteName: 'panker.dev',
 		locale: 'en_US',
-		images: [
-			{
-				url: 'https://cdn.prod.website-files.com/666c7f094ef448a50760aaec/67701d421d4895759ea04b84_opengraph-image.png',
-				width: 1200,
-				height: 630,
-				alt: 'Harel Panker | Frontend Developer',
-			},
-		],
 	},
 	twitter: {
 		card: 'summary_large_image',
 		title: 'Harel Panker | Frontend Developer',
 		description:
 			'Expert frontend developer specializing in React, Next.js, and modern web technologies. Creating responsive, performant, and user-friendly web applications.',
-		images: [
-			'https://cdn.prod.website-files.com/666c7f094ef448a50760aaec/67701d421d4895759ea04b84_opengraph-image.png',
-		],
 	},
 	robots: {
 		index: true,
