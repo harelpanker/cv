fd6f6833314069c0a79e1f99d4be958818575553
Harel Panker <harelpanker@gmail.com>
2026-03-22 09:45:50 +0100

    removing permit

--- lib/clients-data.ts
+++ lib/clients-data.ts
@@ -1,37 +1,62 @@
-import { StaticImageData } from 'next/image';
-import alta from '@/public/images/clients/alta.svg';
-import antidote from '@/public/images/clients/antidote.svg';
-import everafter from '@/public/images/clients/everafter.svg';
-import groundcover from '@/public/images/clients/groundcover.svg';
-import port from '@/public/images/clients/port.svg';
-import permit from '@/public/images/clients/permit.svg';
-import lin from '@/public/images/clients/lin.svg';
-import obol from '@/public/images/clients/obol.svg';
+import { StaticImageData } from "next/image";
+import alta from "@/public/images/clients/alta.svg";
+import antidote from "@/public/images/clients/antidote.svg";
+import everafter from "@/public/images/clients/everafter.svg";
+import groundcover from "@/public/images/clients/groundcover.svg";
+import port from "@/public/images/clients/port.svg";
+import permit from "@/public/images/clients/permit.svg";
+import lin from "@/public/images/clients/lin.svg";
+import obol from "@/public/images/clients/obol.svg";
 
 export type Client = {
-	name: string;
-	logo: StaticImageData;
-	fullUrl: string;
-	showName: string;
+  name: string;
+  logo: StaticImageData;
+  fullUrl: string;
+  showName: string;
 };
 
 export const data: Client[] = [
-	{ name: 'Alta', logo: alta, fullUrl: 'https://www.altahq.com/', showName: 'Alta - altahq.com' },
-	// {
-	// 	name: 'Antidote',
-	// 	logo: antidote,
-	// 	fullUrl: 'https://www.antidotehealth.com/',
-	// 	showName: 'Antidote - antidotehealth.com',
-	// },
-	{ name: 'Everafter', logo: everafter, fullUrl: 'https://www.everafter.ai/', showName: 'Everafter - everafter.ai' },
-	{
-		name: 'Groundcover',
-		logo: groundcover,
-		fullUrl: 'https://www.groundcover.com/',
-		showName: 'Groundcover - groundcover.com',
-	},
-	{ name: 'Port', logo: port, fullUrl: 'https://www.port.io/', showName: 'Port - port.io' },
-	{ name: 'Permit', logo: permit, fullUrl: 'https://www.permit.io/', showName: 'Permit - permit.io' },
-	{ name: 'Lin', logo: lin, fullUrl: 'https://www.lin.health/', showName: 'Lin - lin.health' },
-	{ name: 'Obol', logo: obol, fullUrl: 'https://www.obol.app/', showName: 'Obol - obol.app' },
+  {
+    name: "Alta",
+    logo: alta,
+    fullUrl: "https://www.altahq.com/",
+    showName: "Alta - altahq.com",
+  },
+  // {
+  // 	name: 'Antidote',
+  // 	logo: antidote,
+  // 	fullUrl: 'https://www.antidotehealth.com/',
+  // 	showName: 'Antidote - antidotehealth.com',
+  // },
+  {
+    name: "Everafter",
+    logo: everafter,
+    fullUrl: "https://www.everafter.ai/",
+    showName: "Everafter - everafter.ai",
+  },
+  {
+    name: "Groundcover",
+    logo: groundcover,
+    fullUrl: "https://www.groundcover.com/",
+    showName: "Groundcover - groundcover.com",
+  },
+  {
+    name: "Port",
+    logo: port,
+    fullUrl: "https://www.port.io/",
+    showName: "Port - port.io",
+  },
+  // { name: 'Permit', logo: permit, fullUrl: 'https://www.permit.io/', showName: 'Permit - permit.io' },
+  {
+    name: "Lin",
+    logo: lin,
+    fullUrl: "https://www.lin.health/",
+    showName: "Lin - lin.health",
+  },
+  {
+    name: "Obol",
+    logo: obol,
+    fullUrl: "https://www.obol.app/",
+    showName: "Obol - obol.app",
+  },
 ];
--- lib/projects-data.ts
+++ lib/projects-data.ts
@@ -1,32 +1,73 @@
-import { StaticImageData } from 'next/image';
-import groundcover from '@/public/images/sites/groundcover.png';
-import port from '@/public/images/sites/port.png';
-import permit from '@/public/images/sites/permit.png';
+import { StaticImageData } from "next/image";
+import groundcover from "@/public/images/sites/groundcover.png";
+import port from "@/public/images/sites/port.png";
+import permit from "@/public/images/sites/permit.png";
 
 export type Project = {
-	name: string;
-	fullUrl: string;
-	showName: string;
-	src?: StaticImageData;
+  name: string;
+  fullUrl: string;
+  showName: string;
+  src?: StaticImageData;
 };
 
 export const data: Project[] = [
-	{ name: 'Port', src: port, fullUrl: 'https://www.port.io/', showName: 'Port - port.io' },
-	{ name: 'Permit', src: permit, fullUrl: 'https://www.permit.io/', showName: 'Permit - permit.io' },
-	{
-		name: 'Groundcover',
-		src: groundcover,
-		fullUrl: 'https://www.groundcover.com/',
-		showName: 'groundcover - groundcover.com',
-	},
-	{ name: 'Obol', fullUrl: 'https://www.obol.app/', showName: 'Obol - obol.app' },
-	{ name: 'Everafter', fullUrl: 'https://www.everafter.ai/', showName: 'Everafter - everafter.ai' },
-	{ name: 'Getfabric', fullUrl: 'https://www.getfabric.com/', showName: 'Getfabric - getfabric.com' },
-	{ name: 'Alta', fullUrl: 'https://www.altahq.com/', showName: 'Alta - altahq.com' },
-	{ name: 'Conifers', fullUrl: 'https://www.conifers.ai/', showName: 'Conifers - conifers.ai' },
-	// { name: 'Antidote', fullUrl: 'https://www.antidotehealth.com/', showName: 'Antidote - antidotehealth.com' },
-	{ name: 'Lin', fullUrl: 'https://www.lin.health/', showName: 'Lin - lin.health' },
-	{ name: 'Product @ Port', fullUrl: 'https://productmanagement.port.io/', showName: 'Product management @ Port' },
-	{ name: 'Mensoi', fullUrl: 'https://mensoi.com/', showName: 'Mensoi - mensoi.com' },
-	{ name: 'echo', fullUrl: 'https://www.echohq.com/', showName: 'echo - echohq.com' },
+  {
+    name: "Port",
+    src: port,
+    fullUrl: "https://www.port.io/",
+    showName: "Port - port.io",
+  },
+  // { name: 'Permit', src: permit, fullUrl: 'https://www.permit.io/', showName: 'Permit - permit.io' },
+  {
+    name: "Groundcover",
+    src: groundcover,
+    fullUrl: "https://www.groundcover.com/",
+    showName: "groundcover - groundcover.com",
+  },
+  {
+    name: "Obol",
+    fullUrl: "https://www.obol.app/",
+    showName: "Obol - obol.app",
+  },
+  {
+    name: "Everafter",
+    fullUrl: "https://www.everafter.ai/",
+    showName: "Everafter - everafter.ai",
+  },
+  {
+    name: "Getfabric",
+    fullUrl: "https://www.getfabric.com/",
+    showName: "Getfabric - getfabric.com",
+  },
+  {
+    name: "Alta",
+    fullUrl: "https://www.altahq.com/",
+    showName: "Alta - altahq.com",
+  },
+  {
+    name: "Conifers",
+    fullUrl: "https://www.conifers.ai/",
+    showName: "Conifers - conifers.ai",
+  },
+  // { name: 'Antidote', fullUrl: 'https://www.antidotehealth.com/', showName: 'Antidote - antidotehealth.com' },
+  {
+    name: "Lin",
+    fullUrl: "https://www.lin.health/",
+    showName: "Lin - lin.health",
+  },
+  {
+    name: "Product @ Port",
+    fullUrl: "https://productmanagement.port.io/",
+    showName: "Product management @ Port",
+  },
+  {
+    name: "Mensoi",
+    fullUrl: "https://mensoi.com/",
+    showName: "Mensoi - mensoi.com",
+  },
+  {
+    name: "echo",
+    fullUrl: "https://www.echohq.com/",
+    showName: "echo - echohq.com",
+  },
 ];
